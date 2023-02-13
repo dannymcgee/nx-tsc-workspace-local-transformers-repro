81b7a0e9ab61d9aacca5237980d25207125c89fe
Danny McGee <dannymcgee@gmail.com>
2023-02-12 19:22:50 -0500

    Setup repro

--- packages/transformer/src/index.ts
+++ packages/transformer/src/index.ts
@@ -1,1 +1,25 @@
-export * from './lib/transformer';
+import * as ts from "typescript"
+
+export function before(_: any, program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
+	return context => sourceFile => {
+		const $ = ts.factory
+		return $.updateSourceFile(
+			sourceFile,
+			[
+				$.createExpressionStatement(
+					$.createCallExpression(
+						$.createPropertyAccessExpression(
+							$.createIdentifier("console"),
+							$.createIdentifier("log")
+						),
+						undefined,
+						[
+							$.createStringLiteral("Hello, world!")
+						]
+					)
+				),
+				...sourceFile.statements
+			]
+		)
+	}
+}
--- packages/transformer/src/lib/transformer.ts
+++
@@ -1,3 +0,0 @@
-export function transformer(): string {
-  return 'transformer';
-}
